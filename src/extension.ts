26049018334b9e59e249f8a9d19a6714ad83fd66
Saanchita V <106515879+SaanchitaV@users.noreply.github.com>
2025-08-03 20:02:14 +0000

    Commented code

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,29 @@
 import * as vscode from 'vscode';
+import { RegexPanelProvider } from './panel';
 
-class SimpleViewProvider implements vscode.WebviewViewProvider {
-    constructor(private readonly extensionUri: vscode.Uri) {}
+// class SimpleViewProvider implements vscode.WebviewViewProvider {
+//     constructor(private readonly extensionUri: vscode.Uri) {}
 
-    resolveWebviewView(webviewView: vscode.WebviewView) {
-        webviewView.webview.options = { enableScripts: true };
-        webviewView.webview.html = `
-            <!DOCTYPE html>
-            <html>
-            <body>
-                <h2>Hello from Regex Copilot</h2>
-                <p>This proves the Activity Bar → Webview works.</p>
-            </body>
-            </html>
-        `;
-        console.log("Webview loaded successfully ✅");
-    }
-}
+//     resolveWebviewView(webviewView: vscode.WebviewView) {
+//         webviewView.webview.options = { enableScripts: true };
+//         webviewView.webview.html = `
+//             <!DOCTYPE html>
+//             <html>
+//             <body>
+//                 <h2>Hello from Regex Copilot</h2>
+//                 <p>This proves the Activity Bar → Webview works.</p>
+//             </body>
+//             </html>
+//         `;
+//         console.log("Webview loaded successfully ✅");
+//     }
+// }
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Regex Copilot is active');
 
-    const provider = new SimpleViewProvider(context.extensionUri);
+    // const provider = new SimpleViewProvider(context.extensionUri);
+    const provider = new RegexPanelProvider(context.extensionUri);
 
     context.subscriptions.push(
         vscode.window.registerWebviewViewProvider('regex-copilot-view', provider)
